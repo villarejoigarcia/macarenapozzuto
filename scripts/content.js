bd69eba6410d6ec39015770bb4128443481876bb
Alex Villarejo <axvgarcia@Alexs-MacBook-Pro.local>
2025-10-18 19:53:31 +0200

    251018 1953

--- scripts/content.js
+++ scripts/content.js
@@ -25,6 +25,7 @@ const content = {
         category: "Brand identity",
         year: "2025"
       },
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque odio, accumsan at mauris laoreet, feugiat convallis lacus. Aliquam mattis, diam id sodales finibus, felis lacus accumsan eros, nec venenatis mi orci sit amet eros. Aliquam a tellus augue. Integer et tellus odio.",
       media: [
         { type: "image", src: "images/macarenapozzuto_fotosprint_1.webp" },
         { type: "image", src: "images/macarenapozzuto_fotosprint_4.webp" },
@@ -38,7 +39,12 @@ const content = {
         category: "Campaign",
         year: "2024"
       },
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque odio, accumsan at mauris laoreet, feugiat convallis lacus. Aliquam mattis, diam id sodales finibus, felis lacus accumsan eros, nec venenatis mi orci sit amet eros. Aliquam a tellus augue. Integer et tellus odio.",
       media: [
+        { type: "image", src: "images/macarenapozzuto_orgull_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_orgull_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_orgull_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_orgull_1.webp" },
         { type: "image", src: "images/macarenapozzuto_orgull_1.webp" }
       ]
     },
@@ -48,7 +54,12 @@ const content = {
         category: "Editorial",
         year: "2024"
       },
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque odio, accumsan at mauris laoreet, feugiat convallis lacus. Aliquam mattis, diam id sodales finibus, felis lacus accumsan eros, nec venenatis mi orci sit amet eros. Aliquam a tellus augue. Integer et tellus odio.",
       media: [
+        { type: "image", src: "images/macarenapozzuto_milankundera_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_milankundera_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_milankundera_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_milankundera_1.webp" },
         { type: "image", src: "images/macarenapozzuto_milankundera_1.webp" }
       ]
     },
@@ -58,7 +69,12 @@ const content = {
         category: "Brand identity",
         year: "2024"
       },
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque odio, accumsan at mauris laoreet, feugiat convallis lacus. Aliquam mattis, diam id sodales finibus, felis lacus accumsan eros, nec venenatis mi orci sit amet eros. Aliquam a tellus augue. Integer et tellus odio.",
       media: [
+        { type: "image", src: "images/macarenapozzuto_hero_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_hero_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_hero_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_hero_1.webp" },
         { type: "image", src: "images/macarenapozzuto_hero_1.webp" }
       ]
     },
@@ -68,7 +84,12 @@ const content = {
         category: "Brand identity",
         year: "2023"
       },
+      excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque odio, accumsan at mauris laoreet, feugiat convallis lacus. Aliquam mattis, diam id sodales finibus, felis lacus accumsan eros, nec venenatis mi orci sit amet eros. Aliquam a tellus augue. Integer et tellus odio.",
       media: [
+        { type: "image", src: "images/macarenapozzuto_dingonatura_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_dingonatura_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_dingonatura_1.webp" },
+        { type: "image", src: "images/macarenapozzuto_dingonatura_1.webp" },
         { type: "image", src: "images/macarenapozzuto_dingonatura_1.webp" }
       ]
     }
